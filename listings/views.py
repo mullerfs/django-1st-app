b3fd43c230142c998fc46096514d79dbc058cf55
root <root@_HOSTNAME_>
2021-12-15 19:39:49 +0100

    adding navbar

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Listings
+from .forms import ListingForm
 
 # Create your views here.
 def index(request):
@@ -9,3 +10,15 @@ def all_listings(request):
     all_listings = Listings.objects.order_by('-list_date')
     context = {'all_listings': all_listings}
     return render(request, 'listings/all_listings.html', context)
+
+def new_listing(request):
+    if request.method != 'POST':
+        form = ListingForm()
+    else:
+        form = ListingForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('listings:all_listings')
+      
+    context = {'form': form}
+    return render(request, 'listings/new_listing.html', context)
